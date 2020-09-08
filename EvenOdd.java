7bb843f4377de2cc7256610fe2e88877675718f3
Prakash Kakade <prakash.kakde1993@gmail.com>
2020-09-08 23:47:39 +0530

    Even odd number checking

---
+++ EvenOdd.java
@@ -0,0 +1,14 @@
+public class EvenOdd{
+public static void main(String [] args){
+int num=10;
+if(num%2==0){
+System.out.println("the number is even:"+num);
+
+}else{
+System.out.println("The number is odd"+num);
+
+}
+}
+
+}
+ 
